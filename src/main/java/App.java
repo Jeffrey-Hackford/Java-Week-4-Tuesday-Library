0a9b37371e3ca0c3c48d29d40d796e13516a80bb
Elissa Sosland and Jeffrey Hackford <jeffrey@>
2016-05-10 16:42:55 -0700

    add integration tests and front end vtl files for author list and book list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,30 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/authors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String newAuthor = request.queryParams("AuthorName");
+      model.put("authors", Author.all());
+      model.put("template", "templates/authors.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // get("/authors/:id", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //   Author author = Author.find(Integer.parseInt(request.params(":id")));
+    //   model.put("author", author);
+    //   model.put("allBooks", Book.all());
+    //   model.put("template", "templates/author.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+
+
+    post("/books", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("books", Book.all());
+      model.put("template", "templates/books.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,25 +1,44 @@
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-//
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//
-//   @Override
-//   public WebDriver getDefaultDriver() {
-//     return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//   @Test
-//   public void rootTest() {
-//     goTo("http://localhost:4567/");
-//     assertThat(pageSource()).contains("Triangle finder");
-//   }
-// }
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.sql2o.*;
+import org.junit.*;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Library");
+  }
+
+  @Test
+  public void authorIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#AuthorName").with("Author 1");
+    submit(".Author");
+    assertThat(pageSource()).contains("Authors 1");
+  }
+
+  @Test
+  public void bookIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#BookName").with("Book 1");
+    submit(".Book");
+    assertThat(pageSource()).contains("Book 1");
+  }
+}
